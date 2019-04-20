4fe26070013f922272d04bf3a4312cd6f0bbd2c6
Sergio Gómez <sergio@uco.es>
2019-04-20 11:20:46 +0200

    Added css and addons to storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,9 +1,13 @@
-import { configure } from "@storybook/react";
+import centered from "@storybook/addon-centered";
+import { withKnobs } from "@storybook/addon-knobs/react";
+import { configure, addDecorator } from "@storybook/react";
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context("../../stories", true, /\.stories\.tsx$/);
-function loadStories() {
+const loadStories = () => {
   req.keys().forEach(filename => req(filename));
-}
+};
 
+addDecorator(centered);
+addDecorator(withKnobs);
 configure(loadStories, module);
