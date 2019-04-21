1f561c8cdea4e90e9f38dfd4b2ddd3846127c313
Sergio Gómez <sergio@uco.es>
2019-04-21 16:57:28 +0200

    Added react-time-ago component

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -2,6 +2,13 @@ import centered from "@storybook/addon-centered";
 import { withKnobs } from "@storybook/addon-knobs/react";
 import { configure, addDecorator } from "@storybook/react";
 
+import JavascriptTimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+import es from "javascript-time-ago/locale/es";
+
+JavascriptTimeAgo.locale(en);
+JavascriptTimeAgo.locale(es);
+
 // automatically import all files ending in *.stories.tsx
 const req = require.context("../../stories", true, /\.stories\.tsx$/);
 const loadStories = () => {
