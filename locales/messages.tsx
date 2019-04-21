dccc97dba66c48850246bd36665aa1264e4d0a0f
Sergio Gómez <sergio@uco.es>
2019-04-22 01:05:58 +0200

    Added react-intl for translations
    Removed react-time-ago

---
+++ locales/messages.tsx
@@ -0,0 +1,21 @@
+// Provide your messages
+export const messages = {
+  en: {
+    "idea.send": `Send by {user}`,
+    "idea.state.new": "NEW",
+    "idea.state.pending": "PENDING",
+    "idea.state.approved": "APPROVED",
+    "idea.state.rejected": "REJECTED",
+    "idea.extra.users": `{count}/{seats} {count, plural, one {person} other {people}}`,
+    "idea.extra.users.unlimited": `{count} {count, plural, one {person} other {people}}`
+  },
+  es: {
+    "idea.send": `Enviado por {user}`,
+    "idea.state.new": "NUEVA",
+    "idea.state.pending": "PENDIENTE",
+    "idea.state.approved": "APROBADA",
+    "idea.state.rejected": "RECHAZADA",
+    "idea.extra.users": `{count}/{seats} {count, plural, one {persona} other {personas}}`,
+    "idea.extra.users.unlimited": `{count} {count, plural, one {persona} other {personas}}`
+  }
+};
