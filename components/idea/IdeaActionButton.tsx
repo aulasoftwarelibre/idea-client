b1c5a3b1231d3f11be27203f5e9f1374fb840df9
Sergio Gómez <sergio@uco.es>
2019-04-22 21:12:35 +0200

    Added closed attribute in IdeaActionButton
    Moved from parent for simplicity

--- components/idea/IdeaActionButton.tsx
+++ components/idea/IdeaActionButton.tsx
@@ -1,37 +1,47 @@
-import * as React from "react";
-import { Button, Icon } from "semantic-ui-react";
+import * as React from 'react';
+import { Button, Icon } from 'semantic-ui-react';
+import { FormattedMessage } from 'react-intl';
 
 export const IdeaActionButton = ({
+  closed,
   joined,
   handleOnJoin,
   handleOnLeave
-}: Props): JSX.Element =>
-  joined ? (
-    <Button
-      compact
-      animated
-      icon
-      color="green"
-      size={"small"}
-      onClick={() => handleOnLeave()}
-    >
-      <Button.Content visible>
-        <Icon name="check" />
-        Joined
-      </Button.Content>
-      <Button.Content hidden>
-        <Icon name="arrow left" />
-        Leave
-      </Button.Content>
-    </Button>
-  ) : (
-    <Button compact icon primary size={"small"} onClick={() => handleOnJoin()}>
+}: Props): JSX.Element => {
+  if (closed) {
+    return (
+      <Button icon compact color={'red'} size={'small'} disabled>
+        <Icon name="close" />
+        <FormattedMessage id="idea.action.closed" />
+      </Button>
+    );
+  }
+
+  if (joined) {
+    return (
+      <Button compact animated icon color="green" size="small" onClick={() => handleOnLeave()}>
+        <Button.Content visible>
+          <Icon name="check" />
+          <FormattedMessage id="idea.action.joined" />
+        </Button.Content>
+        <Button.Content hidden>
+          <Icon name="arrow left" />
+          <FormattedMessage id="idea.action.leave" />
+        </Button.Content>
+      </Button>
+    );
+  }
+
+  return (
+    <Button compact icon primary size={'small'} onClick={() => handleOnJoin()}>
       <Icon name="add" />
-      Join
+      <FormattedMessage id="idea.action.join" />
     </Button>
   );
+};
 
 export interface Props {
+  closed: boolean;
   joined: boolean;
   handleOnJoin: any;
   handleOnLeave: any;
--- locales/messages.tsx
+++ locales/messages.tsx
@@ -1,21 +1,29 @@
 // Provide your messages
 export const messages = {
   en: {
-    "idea.send": `Send by {user}`,
-    "idea.state.new": "NEW",
-    "idea.state.pending": "PENDING",
-    "idea.state.approved": "APPROVED",
-    "idea.state.rejected": "REJECTED",
-    "idea.extra.users": `{count}/{seats} {count, plural, one {person} other {people}}`,
-    "idea.extra.users.unlimited": `{count} {count, plural, one {person} other {people}}`
+    'idea.action.closed': 'Closed',
+    'idea.action.join': 'Join',
+    'idea.action.joined': 'Joined',
+    'idea.action.leave': 'Leave',
+    'idea.send': `Send by {user}`,
+    'idea.state.new': 'NEW',
+    'idea.state.pending': 'PENDING',
+    'idea.state.approved': 'APPROVED',
+    'idea.state.rejected': 'REJECTED',
+    'idea.extra.users': `{count}/{seats} {count, plural, one {person} other {people}}`,
+    'idea.extra.users.unlimited': `{count} {count, plural, one {person} other {people}}`
   },
   es: {
-    "idea.send": `Enviado por {user}`,
-    "idea.state.new": "NUEVA",
-    "idea.state.pending": "PENDIENTE",
-    "idea.state.approved": "APROBADA",
-    "idea.state.rejected": "RECHAZADA",
-    "idea.extra.users": `{count}/{seats} {count, plural, one {persona} other {personas}}`,
-    "idea.extra.users.unlimited": `{count} {count, plural, one {persona} other {personas}}`
+    'idea.action.closed': 'Cerrado',
+    'idea.action.join': 'Unirte',
+    'idea.action.joined': 'Unido',
+    'idea.action.leave': 'Salir',
+    'idea.send': `Enviado por {user}`,
+    'idea.state.new': 'NUEVA',
+    'idea.state.pending': 'PENDIENTE',
+    'idea.state.approved': 'APROBADA',
+    'idea.state.rejected': 'RECHAZADA',
+    'idea.extra.users': `{count}/{seats} {count, plural, one {persona} other {personas}}`,
+    'idea.extra.users.unlimited': `{count} {count, plural, one {persona} other {personas}}`
   }
 };
