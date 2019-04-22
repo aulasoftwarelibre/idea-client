787b90d75d8d049d182d32b7d4121c02241d6a5c
Sergio Gómez <sergio@uco.es>
2019-04-22 21:13:39 +0200

    Refactorized IdeaItem
    Changed to styled-component

--- components/idea/IdeaItem.tsx
+++ components/idea/IdeaItem.tsx
@@ -1,10 +1,12 @@
-import * as React from "react";
-import { Card, Icon, Button, Label } from "semantic-ui-react";
-import { FormattedMessage, FormattedRelative } from "react-intl";
+import * as React from 'react';
+import { Card, Label } from 'semantic-ui-react';
+import { FormattedMessage, FormattedRelative } from 'react-intl';
+import styled, { css } from 'styled-components';
+import Parser from 'html-react-parser';
 
-import { IdeaExtraItem } from "./IdeaExtraItem";
-import { IdeaActionButton } from "./IdeaActionButton";
-import { Idea } from "../../model/Idea";
+import { IdeaExtraItem } from './IdeaExtraItem';
+import { IdeaActionButton } from './IdeaActionButton';
+import { Idea } from '../../model/Idea';
 
 export const IdeaItem = ({
   idea,
@@ -13,97 +15,39 @@ export const IdeaItem = ({
   handleOnJoin,
   handleOnLeave
 }: Props): JSX.Element => (
-  <Card
-    fluid={true}
-    {...(summary ? { onClick: () => handleOnTitleClick(idea.slug) } : {})}
-  >
-    <Card.Content>
-      <Card.Meta className="send by">
-        <span className="group">{idea.group.name}</span>
-        <FormattedMessage
-          id="idea.send"
-          values={{
-            user: idea.owner.username
-          }}
-        />
+  <IdeaBox fluid>
+    <IdeaContent
+      {...(summary ? { clickable: true, onClick: () => handleOnTitleClick(idea.slug) } : {})}
+    >
+      <IdeaSendBy>
+        <IdeaGroupName>{idea.group.name}</IdeaGroupName>
+        <FormattedMessage id="idea.send" values={{ user: idea.owner.username }} />
         <FormattedRelative value={idea.createdAt} />
-      </Card.Meta>
+      </IdeaSendBy>
       <Card.Header>{idea.title}</Card.Header>
-      <Label attached={"top right"} inverted color={"green"}>
+      <Label attached={'top right'} inverted color={'green'}>
         <FormattedMessage id={`idea.state.${idea.state}`} />
       </Label>
-      <Card.Description>
-        <div
-          className={summary ? "summary" : ""}
-          dangerouslySetInnerHTML={{ __html: idea.description }}
-        />
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      {idea.closed ? (
-        <Button icon compact color={"red"} size={"small"} disabled>
-          <Icon name="close" />
-          Closed
-        </Button>
-      ) : (
-        <IdeaActionButton
-          joined={!!idea.joined}
-          handleOnJoin={handleOnJoin}
-          handleOnLeave={handleOnLeave}
-        />
-      )}
+      <IdeaDescription summary={summary}>{Parser(idea.description)}</IdeaDescription>
+    </IdeaContent>
+    <IdeaContent extra>
+      <IdeaActionButton
+        closed={idea.closed}
+        joined={!!idea.joined}
+        handleOnJoin={handleOnJoin}
+        handleOnLeave={handleOnLeave}
+      />
       <IdeaExtraItem icon="user">
-        {idea.numSeats ? (
-          <FormattedMessage
-            id="idea.extra.users"
-            values={{
-              count: idea.votes.length,
-              seats: idea.numSeats
-            }}
-          />
-        ) : (
-          <FormattedMessage
-            id="idea.extra.users.unlimited"
-            values={{
-              count: idea.votes.length
-            }}
-          />
-        )}
+        <FormattedMessage
+          id={idea.numSeats ? 'idea.extra.users' : 'idea.extra.users.unlimited'}
+          values={{
+            count: idea.votes.length,
+            seats: idea.numSeats
+          }}
+        />
       </IdeaExtraItem>
-    </Card.Content>
-    <style jsx>{`
-      .ui.card {
-        border-radius: 0;
-      }
-      .ui.card > .content > .description {
-        clear: unset;
-        font-family: "Lato", sans-serif;
-      }
-      .title {
-        cursor: pointer;
-      }
-      .summary {
-        -webkit-mask-image: linear-gradient(180deg, #000 60%, transparent);
-        mask-image: linear-gradient(180deg, #000 60%, transparent);
-        overflow: hidden;
-        padding: 5px 8px 10px;
-        max-height: 200px;
-      }
-      .group {
-        background-color: #edeff1;
-        padding: 0 4px;
-        border-radius: 2px;
-        font-weight: 500;
-        color: black;
-      }
-      .send.by {
-        margin-bottom: 10px;
-      }
-      .ui.card > .extra {
-        border-top: unset !important;
-      }
-    `}</style>
-  </Card>
+    </IdeaContent>
+  </IdeaBox>
 );
 
 export interface Props {
@@ -113,3 +57,48 @@ export interface Props {
   handleOnJoin: any;
   handleOnLeave: any;
 }
+
+const IdeaBox = styled(Card)`
+  &.ui.card {
+    border-radius: 0;
+  }
+`;
+
+const IdeaContent = styled(Card.Content)`
+  ${props =>
+    props.clickable &&
+    css`
+      cursor: pointer;
+    `}
+
+  &.content.extra {
+    border-top: unset !important;
+  }
+`;
+
+const IdeaSendBy = styled(Card.Meta)`
+  margin-bottom: 10px;
+`;
+
+const IdeaGroupName = styled.span`
+  background-color: #edeff1;
+  padding: 0 4px;
+  border-radius: 2px;
+  font-weight: 500;
+  color: black;
+`;
+
+const IdeaDescription = styled(Card.Description)`
+  clear: unset;
+  font-family: 'Lato', sans-serif;
+
+  ${props =>
+    props.summary &&
+    css`
+      -webkit-mask-image: linear-gradient(180deg, #000 60%, transparent);
+      mask-image: linear-gradient(180deg, #000 60%, transparent);
+      overflow: hidden;
+      padding: 5px 8px 10px;
+      max-height: 200px;
+    `}
+`;
